34ec4c54da4704bcb59ee54e424e158d8bed2df5
zxing <zxing@zxingdeMacBook-Pro.local>
2017-04-22 20:12:21 +0800

    first commit

---
+++ toTop/BaseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseTableViewCell.swift
+//  toTop
+//
+//  Created by zxing on 2017/4/22.
+//  Copyright © 2017年 zxing. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ toTop/String.swift
@@ -0,0 +1,9 @@
+//
+//  String.swift
+//  toTop
+//
+//  Created by zxing on 2017/4/22.
+//  Copyright © 2017年 zxing. All rights reserved.
+//
+
+import Foundation
